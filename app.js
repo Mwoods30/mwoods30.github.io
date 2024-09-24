36ddf24d0c97cf4c940baf967a7624e29694b0ea
Mwoods30 <128528136+Mwoods30@users.noreply.github.com>
2024-09-24 15:03:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+const menu = document.getElementById('mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
